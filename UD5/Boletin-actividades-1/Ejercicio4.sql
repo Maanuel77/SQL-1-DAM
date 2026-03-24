98bcfecb1ff5c335bcda16f2e91d38defd01f4d8
Manuel Martinez <martinez.maman25@triana.salesianos.edu>
2026-03-24 13:38:19 +0100

    Practicas northwind sesion 7 y ejercicio 4 del boletin

---
+++ UD5/Boletin-actividades-1/Ejercicio4.sql
@@ -0,0 +1,17 @@
+/*
+Seleccionar el vuelo más largo (con mayor duración)
+de cada día de la semana. Debe aparecer el nombre 
+del aeropuerto de salida, el de llegada, la fecha y
+hora de salida y llegada y la duración.
+*/
+select v1.desde, v1.hasta, v1.salida, v1.llegada,
+	age(v1.llegada, v1.salida) "duracion",
+	extract (isodow from v1.salida) "dia_seman
+	a"
+from vuelo v1
+where age (llegada, salida) >= all (
+	select age(v2.llegada,v2.salida)
+	from vuelo v2
+	where extract (isodow from v1.salida) = 
+		  extract (isodow from v2.salida)
+)
---
+++ UD5/norhtwind_practicas_clase_sesion7.sql
@@ -0,0 +1,19 @@
+-- Seleccionar el producto más caro de cada categoría
+-- category_name, product_name, unit_price
+
+select category_name, product_name, unit_price
+from products p1 join categories using (category_id)
+where p1.unit_price >= all (
+	select p2.unit_price
+	from products p2
+	where p1.category_id = p2.category_id
+)
+
+SELECT p1.product_name, p1.unit_price, p1.category_id
+FROM products p1
+WHERE p1.unit_price >
+(
+    SELECT AVG(p2.unit_price)
+    FROM products p2
+    WHERE p2.category_id = p1.category_id
+);
